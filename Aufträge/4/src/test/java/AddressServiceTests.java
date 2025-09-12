9a23b89b3346d1d139b880cab1a7db7bef2fa39c
ezpcy <batu.s@live.com>
2025-09-12 09:58:32 +0200

    Update

---
+++ Aufträge/4/src/test/java/AddressServiceTests.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ch.tbz.m450.service.AddressService;
+
+public class AddressServiceTests {
+  AddressService adSer;
+
+  @BeforeEach
+
+  @Test
+  public void testSave() {
+    assertTrue(true);
+  }
+
+}
